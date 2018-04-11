363d795658c3f58c3d557b01cb56f2f96f05c4a1
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-11 19:34:49 +1000

    Test no diffusion of particles with potential
    Particles which are trapped within the harmonic potential should have
    no, or very little diffusion, if the algorithm is properly trapping the
    particles in the potential, i.e. things like the force are in the
    appropriate direction.

--- test/test_harmonic_force.py
+++ test/test_harmonic_force.py
@@ -9,16 +9,17 @@
 """Test appplying the harmonic force to a simulation."""
 
 import hoomd
+import numpy as np
 import pytest
 from hoomd.harmonic_force import HarmonicForceCompute
 
-hoomd.context.initialize()
 
 @pytest.fixture
 def square_lattice():
-    return hoomd.init.create_lattice(
-        unitcell=hoomd.lattice.sq(a=2.0),
-        n=5)
+    with hoomd.context.initialize():
+        yield hoomd.init.create_lattice(
+            unitcell=hoomd.lattice.sq(a=2.0),
+            n=5)
 
 
 def test_simple(square_lattice):
@@ -30,3 +31,21 @@ def test_simple(square_lattice):
         1.
     )
     hoomd.run(10)
+
+def test_no_diffusion(square_lattice):
+    snap_init = square_lattice.take_snapshot()
+    pairs = hoomd.md.pair.lj(2.5, nlist=hoomd.md.nlist.cell())
+    pairs.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
+    HarmonicForceCompute(
+        hoomd.group.all(),
+        [tuple(row) for row in snap_init.particles.position],
+        [tuple(row) for row in snap_init.particles.orientation],
+        1.
+    )
+    hoomd.md.update.enforce2d()
+    hoomd.md.integrate.mode_standard(dt=0.005)
+    hoomd.md.integrate.nvt(hoomd.group.all(), kT=1, tau=1)
+    hoomd.run(1000)
+    snap_final = square_lattice.take_snapshot()
+    assert np.linalg.norm(snap_final.particles.position - snap_init.particles.position).mean() < 0.01
+
