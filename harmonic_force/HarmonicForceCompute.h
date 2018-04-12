e448949d7f426bc9eb65a46fbe33029c068546d5
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-12 15:42:42 +1000

    Format cpp files using clang-format
    I don't like making decisions about formatting, especially in a language
    I don't particularly know well, especially the intricacies of what is
    appropriate in terms of code format. To this end I am using clang-format
    to decide for me, using the default Google style.

--- harmonic_force/HarmonicForceCompute.h
+++ harmonic_force/HarmonicForceCompute.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2018 Malcolm Ramsay
 
-
 // inclusion guard
 #ifndef _HARMONIC_FORCE_COMPUTE_H_
 #define _HARMONIC_FORCE_COMPUTE_H_
@@ -17,43 +16,38 @@
 #include <hoomd/extern/pybind/include/pybind11/pybind11.h>
 #endif
 
+class HarmonicForceCompute : public ForceCompute {
+ public:
+  //! Constructor
+  HarmonicForceCompute(std::shared_ptr<SystemDefinition> sysdef,
+                       std::shared_ptr<ParticleGroup> group,
+                       pybind11::list p_lst, pybind11::list o_lst,
+                       Scalar translational_force_constant,
+                       Scalar rotational_force_constant);
 
-class HarmonicForceCompute : public ForceCompute
-    {
-    public:
-        //! Constructor
-        HarmonicForceCompute(std::shared_ptr<SystemDefinition> sysdef,
-                             std::shared_ptr<ParticleGroup> group,
-                             pybind11::list p_lst, pybind11::list o_lst,
-                             Scalar translational_force_constant,
-                             Scalar rotational_force_constant
-                             );
+  //! Destructor
+  ~HarmonicForceCompute();
 
-        //! Destructor
-        ~HarmonicForceCompute();
+ protected:
+  //! Set the force to a new value
+  virtual void setForces();
 
+  //! Actually compute the forces
+  virtual void computeForces(unsigned int timestep);
 
-    protected:
-        //! Set the force to a new value
-        virtual void setForces();
+  std::shared_ptr<ParticleGroup>
+      m_group;  //!< Group of particles on which this force is applied
 
-        //! Actually compute the forces
-        virtual void computeForces(unsigned int timestep);
+  GPUArray<Scalar3> m_position_lattice;
+  GPUArray<Scalar4> m_orientation_lattice;
 
-        std::shared_ptr<ParticleGroup> m_group;   //!< Group of particles on which this force is applied
+  Scalar translational_force_constant;
+  Scalar rotational_force_constant;
 
-        GPUArray<Scalar3> m_position_lattice;
-        GPUArray<Scalar4> m_orientation_lattice;
-
-        Scalar translational_force_constant;
-        Scalar rotational_force_constant;
-
-        unsigned int last_computed;
-
-    };
+  unsigned int last_computed;
+};
 
 //! Exports the ActiveForceComputeClass to python
 void export_HarmonicForceCompute(pybind11::module& m);
 
-
-#endif // _HARMONIC_FORCE_COMPUTE_H_
+#endif  // _HARMONIC_FORCE_COMPUTE_H_
