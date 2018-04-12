4f272481f4ccb44f6f7975d9fd3247219eb119a1
Malcolm Ramsay <malramsay64@gmail.com>
2018-04-12 12:01:00 +1000

    Use single fixture for many simulation setups
    Rather than having separate fixtures for each type of experiment, I have
    created a single fixture that uses parameters to enable many different
    types of simulations and testing of the behaviour of the force.
    
    New tests include ensuring that when the positions initially set away
    from the expected, they will return to the expected values.

--- test/test_harmonic_force.py
+++ test/test_harmonic_force.py
@@ -8,23 +8,17 @@
 
 """Test appplying the harmonic force to a simulation."""
 
+import itertools
+
 import hoomd
 import numpy as np
 import pytest
 from hoomd.harmonic_force import HarmonicForceCompute
 
 
-@pytest.fixture
-def square_lattice():
-    with hoomd.context.initialize():
-        yield hoomd.init.create_lattice(unitcell=hoomd.lattice.sq(a=2.0), n=5)
-
-
-@pytest.fixture
-def simulation_setup():
-    delta = 0
-    create_rigid = False
-    with hoomd.context.initialize():
+@pytest.fixture(params=itertools.product([-0.1, 0, 0.1, 0.4], [True, False]))
+def simulation_setup(delta: float = 0, create_rigid: bool = False):
+    with hoomd.context.initialize(''):
         unit_cell = hoomd.lattice.unitcell(
             N=1,
             a1=[2, 0, 0],
@@ -32,71 +26,47 @@ def simulation_setup():
             a3=[0, 0, 1],
             dimensions=2,
             position=[[delta, delta, 0]],
-            type_name=['A'],
+            type_name=["A"],
             mass=[1.0],
             moment_inertia=[[0, 0, 1]],
             orientation=[[1 - delta, 0, 0, delta]],
         )
         system = hoomd.init.create_lattice(unitcell=unit_cell, n=5)
         pairs = hoomd.md.pair.lj(2.5, nlist=hoomd.md.nlist.cell())
-        pairs.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
+        pairs.pair_coeff.set("A", "A", epsilon=1.0, sigma=1.0)
         group = hoomd.group.all()
-        nmols = system.particles.get_metadata()['N']
+        nmols = system.particles.get_metadata()["N"]
         if create_rigid:
-            system.particles.types.add('B')
-            pairs.pair_coeff.set('A', 'B', epsilon=1.0, sigma=1.0)
-            pairs.pair_coeff.set('B', 'B', epsilon=1.0, sigma=1.0)
+            system.particles.types.add("B")
+            pairs.pair_coeff.set("A", "B", epsilon=1.0, sigma=1.0)
+            pairs.pair_coeff.set("B", "B", epsilon=1.0, sigma=1.0)
             rigid = hoomd.md.constrain.rigid()
-            rigid.set_param('A', types=['B'], positions=[(-1, 0, 0)])
+            rigid.set_param("A", types=["B"], positions=[(-1, 0, 0)])
             rigid.create_bodies()
             group = hoomd.group.rigid_center()
         hoomd.md.update.enforce2d()
         hoomd.md.integrate.mode_standard(dt=0.005)
         hoomd.md.integrate.nvt(group, kT=1, tau=1)
-        yield {'system': system, 'group': group, 'nmols': nmols, 'delta': delta}
+        yield {"system": system, "group": group, "nmols": nmols, "delta": delta}
 
 
-def test_simple(square_lattice):
-    snap = square_lattice.take_snapshot()
-    HarmonicForceCompute(
-        hoomd.group.all(),
-        [tuple(row) for row in snap.particles.position],
-        [tuple(row) for row in snap.particles.orientation],
-        1.,
+def test_HarmonicForceCompute(simulation_setup):
+    snap_init = simulation_setup["system"].take_snapshot()
+    nmols = simulation_setup["nmols"]
+    group = simulation_setup["group"]
+    delta = simulation_setup["delta"]
+    intended_position = snap_init.particles.position[:nmols] - np.array(
+        [delta, delta, 0]
     )
-    hoomd.run(10)
-
-
-def test_no_diffusion(square_lattice):
-    snap_init = square_lattice.take_snapshot()
-    pairs = hoomd.md.pair.lj(2.5, nlist=hoomd.md.nlist.cell())
-    pairs.pair_coeff.set("A", "A", epsilon=1.0, sigma=1.0)
-    HarmonicForceCompute(
-        hoomd.group.all(),
-        [tuple(row) for row in snap_init.particles.position],
-        [tuple(row) for row in snap_init.particles.orientation],
-        1.,
+    intended_orientation = snap_init.particles.orientation[:nmols] - np.array(
+        [-delta, 0, 0, delta]
     )
-    hoomd.md.update.enforce2d()
-    hoomd.md.integrate.mode_standard(dt=0.005)
-    hoomd.md.integrate.nvt(hoomd.group.all(), kT=1, tau=1)
+    HarmonicForceCompute(group, intended_position, intended_orientation, 10.)
     hoomd.run(1000)
-    snap_final = square_lattice.take_snapshot()
-    assert np.linalg.norm(
-        snap_final.particles.position - snap_init.particles.position
-    ).mean() < 0.01
-
-def test_no_rotation(simulation_setup):
-    snap_init = simulation_setup['system'].take_snapshot()
-    nmols = simulation_setup['nmols']
-    group = simulation_setup['group']
-    HarmonicForceCompute(group,
-                         snap_init.particles.position[:nmols],
-                         snap_init.particles.orientation[:nmols],
-                         10.
-                         )
-    hoomd.run(1000)
-    snap_final = simulation_setup['system'].take_snapshot()
-    assert np.linalg.norm(
-        snap_final.particles.orientation - snap_init.particles.orientation
-    ).mean() < 0.01
+    snap_final = simulation_setup["system"].take_snapshot()
+    assert np.all(np.linalg.norm(
+        snap_final.particles.position[:nmols] - intended_position
+    ) < 0.01)
+    assert np.all(np.linalg.norm(
+        snap_final.particles.orientation[:nmols] - intended_orientation
+    ) < 0.01)
